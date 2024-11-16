50ab207153cd91753347bdede4e97a84ccb2b803
akshay <akshayajikumar10@gmail.com>
2024-11-16 21:23:12 +0530

    Calculator new commit

---
+++ calculator/script.js
@@ -0,0 +1,38 @@
+function displayNumber(event){
+    var val = event.target.value;
+
+    let operators =["+","-","*", "/", "."];
+
+    let currentExpLastChar=result.value.slice(-1);
+
+    if (operators.includes(currentExpLastChar) && operators.includes(val)){
+        return
+    }
+
+
+    result.value+=val;
+
+}
+
+function clearBox(){
+
+    result.value="";
+}
+
+function evalExpr(){
+
+    let currentExpr=result.value;
+
+    let output=eval(currentExpr);
+
+    result.value=output;
+}
+
+function clearNum(){
+
+
+    result.value=result.value.slice(0,-1);
+    
+    
+
+}
